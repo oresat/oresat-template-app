11d7099c2373ced1386a62d405112a840c4771a9
Pete Skeggs <plskeggs@noeticdesign.com>
2025-12-07 15:34:21 -0800

    WIP
    get to build

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #include <zephyr/sys/reboot.h>
 
 #include <canopennode.h>
-#include <OD.h>
+#include <CO_OD.h>
 #include <board_sensors.h>
 #include <oresat.h>
 
@@ -30,28 +30,60 @@ LOG_MODULE_REGISTER(oresat_zephyr_template, LOG_LEVEL_DBG);
 
 int main(void)
 {
-	k_timepoint_t timepoint;
+	uint16_t timeout;
+	uint32_t elapsed;
+	int64_t timestamp;
+	CO_NMT_reset_cmd_t reset = CO_RESET_NOT;
+	struct canopen_context can = {.dev = CAN_INTERFACE};
 	uint8_t node_id = oresat_get_node_id();
+	int err;
 
 	LOG_INF("Oresat app template starting up on board: %s, node: %u", CONFIG_BOARD_TARGET, (unsigned int)node_id);
 
 	oresat_fix_pdo_cob_ids(node_id);
 
 	LOG_DBG("Opening CAN");
-	canopennode_init(CAN_INTERFACE, CAN_BITRATE, node_id);
+	//canopennode_init(CAN_INTERFACE, CAN_BITRATE, node_id);
+	if (!device_is_ready(can.dev)) {
+		LOG_ERR("CAN interface is not ready");
+		return 0;
+	}
+	err = CO_init(&can, node_id, CAN_BITRATE);
+	if (err != CO_ERROR_NO) {
+		LOG_ERR("CO_init failed (err = %d)", err);
+		return 0;
+	}
+	LOG_INF("CANopen stack initialized");
+
 	LOG_DBG("Initializing sensors");
 	board_sensors_init();
 
 	LOG_DBG("Starting main loop");
-	while (canopennode_is_running()) {
-		timepoint = sys_timepoint_calc(K_MSEC(1000));
-		board_sensors_fill_od();
-		k_sleep(sys_timepoint_timeout(timepoint));
+	elapsed = 0U;
+
+	// CO_OD_configure();
+	CO_CANsetNormalMode(CO->CANmodule[0]);
+
+	while (true) {
+		timeout = 1000U;
+		timestamp = k_uptime_get();
+		reset = CO_process(CO, (uint16_t)elapsed, &timeout);
+		if (reset != CO_RESET_NOT) {
+			break;
+		}
+
+		if (timeout > 0) {
+			board_sensors_fill_od();
+			k_sleep(K_MSEC(timeout));
+			elapsed = (uint32_t)k_uptime_delta;
+		} else {
+			elapsed = 0U;
+		}
 	}
 
-	canopennode_stop(CAN_INTERFACE);
-	sys_reboot(SYS_REBOOT_COLD);
+	CO_delete(&can);
 	LOG_DBG("Done.");
+	sys_reboot(SYS_REBOOT_COLD);
 
 	return 0;
 }
