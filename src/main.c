af04458be0513cf5bcaf268ae673c6a3269f150f
Pete Skeggs <plskeggs@noeticdesign.com>
2025-12-07 15:32:11 -0800

    Port CAN code to native CAN
    Integrate CAN code from old zephyr repo.
    Use to test against a dev board.
    Check in OpenEDSEditor data.
    Also check in generated code, since the editor is Windows-only.

--- src/main.c
+++ src/main.c
@@ -14,15 +14,90 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/sys/reboot.h>
+
+#include <canopennode.h>
+#include <CO_OD.h>
+#include <board_sensors.h>
+#include <oresat.h>
 
 LOG_MODULE_REGISTER(oresat_zephyr_template, LOG_LEVEL_DBG);
 
+#define CAN_INTERFACE (DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus)))
+#define CAN_BITRATE (DT_PROP_OR(DT_CHOSEN(zephyr_canbus), bitrate, \
+					 DT_PROP_OR(DT_CHOSEN(zephyr_canbus), bus_speed, \
+								CONFIG_CAN_DEFAULT_BITRATE) / 1000))
+
 int main(void)
 {
-    LOG_INF("Oresat app template starting up on board: %s", CONFIG_BOARD_TARGET);
+	uint16_t timeout;
+	uint32_t elapsed;
+	int64_t timestamp;
+	CO_NMT_reset_cmd_t reset = CO_RESET_NOT;
+	struct canopen_context can = {.dev = CAN_INTERFACE};
+	uint8_t node_id = oresat_get_node_id();
+	int err;
 
-    printk("Logging from printk\r\n");
-    return 0;
+	LOG_INF("Oresat app template starting up on board: %s, node: %u", CONFIG_BOARD_TARGET, (unsigned int)node_id);
+
+	oresat_fix_pdo_cob_ids(node_id);
+
+	LOG_DBG("Opening CAN");
+	//canopennode_init(CAN_INTERFACE, CAN_BITRATE, node_id);
+	if (!device_is_ready(can.dev)) {
+		LOG_ERR("CAN interface is not ready");
+		return 0;
+	}
+	err = CO_init(&can, node_id, CAN_BITRATE);
+	if (err != CO_ERROR_NO) {
+		LOG_ERR("CO_init failed (err = %d)", err);
+		return 0;
+	}
+	LOG_INF("CANopen stack initialized");
+
+	LOG_DBG("Initializing sensors");
+	board_sensors_init();
+
+	LOG_DBG("Starting main loop");
+	elapsed = 0U;
+
+	// CO_OD_configure();
+	CO_CANsetNormalMode(CO->CANmodule[0]);
+
+	while (true) {
+		timeout = 1000U;
+		timestamp = k_uptime_get();
+
+		bool_t syncWas = false;
+
+		/* Process Sync */
+		//syncWas = CO_process_SYNC(CO, timeout);
+
+		/* Read inputs */
+		CO_process_RPDO(CO, syncWas);
+
+		/* Further I/O or nonblocking application code may go here. */
+
+		/* Write outputs */
+		CO_process_TPDO(CO, syncWas, timeout * 1000U);
+
+		reset = CO_process(CO, (uint16_t)elapsed, &timeout);
+		if (reset != CO_RESET_NOT) {
+			break;
+		}
+
+		if (timeout > 0) {
+			board_sensors_fill_od();
+			k_sleep(K_MSEC(timeout));
+			elapsed = (uint32_t)k_uptime_delta(&timestamp);
+		} else {
+			elapsed = 0U;
+		}
+	}
+
+	CO_delete(&can);
+	LOG_DBG("Done.");
+	sys_reboot(SYS_REBOOT_COLD);
+
+	return 0;
 }
-
-
