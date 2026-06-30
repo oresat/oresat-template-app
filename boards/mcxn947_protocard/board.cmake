61fcd443554e817464784bfe240b4a6565f06928
William Jeffreys <wjeffreys96@gmail.com>
2026-06-29 22:14:14 -0700

    Add speed flags for probe-rs
    add --speed flag when flashing with probe-rs as it will frequently fail
    without it. Also update pyocd frequency flag to match.

--- boards/mcxn947_protocard/board.cmake
+++ boards/mcxn947_protocard/board.cmake
@@ -31,10 +31,11 @@ endif()
 
 # Pyocd support added with the NXP.MCXN947_DFP.17.0.0.pack CMSIS Pack
 board_runner_args(pyocd "--target=mcxn947")
-board_runner_args(pyocd "--frequency=4000000")
+board_runner_args(pyocd "--frequency=5000000")
 
 # Probe-RS support
 board_runner_args(probe-rs "-O=--chip-description-path=${BOARD_DIR}/support/MCXN947.yaml" "--chip=MCXN947")
+board_runner_args(probe-rs "--speed=5000")
 
 include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
