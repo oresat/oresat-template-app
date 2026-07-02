d1b9644d72b3a6c36d6f0ca5143023dc00249dc8
Pete Skeggs <plskeggs@noeticdesign.com>
2026-07-01 17:06:44 -0700

    accept param for ttyACMn

--- can_setup.sh
+++ can_setup.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-echo Setting up can0 on ttyACM0
-sudo slcand -o -c -s8 /dev/ttyACM0 can0
+#!/bin/bash
+n=${1:-0}
+echo opening can0 on /dev/ttyACM$n
+sudo slcand -o -c -s8 /dev/ttyACM$n can0
 sudo ip link set can0 type can bitrate 1000000
 sudo ifconfig can0 up
 
