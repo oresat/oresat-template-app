88c318512f3c6ac440dd8f9da28e5a9ed0a10b46
Pete Skeggs <plskeggs@noeticdesign.com>
2026-07-15 22:41:58 -0700

    Add app version generation
    This uses git rev-parse --short HEAD to obtain a string
    containing the current commit's short hash.
    
    This and some user-assigned symantic version numbers
    are combined using VERSION.in to generate the file
    VERSION in the root of the project.
    
    Zephyr will then build header files for itself as well
    as this app, which can then be used for logging or
    other things.
    
    Modify main.c to display the versions, as well
    as the unique processor hardware id.
    
    These changes make the template app behave in a uniform
    manner with the other apps, such as battery, mag, and solar.

--- src/main.c
+++ src/main.c
@@ -12,21 +12,56 @@
  * editor.
  */
 #include <zephyr/device.h>
-#include <zephyr/dfu/mcuboot.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/hwinfo.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/reboot.h>
-
-#include <CANopen.h>
-#include <canopennode.h>
-
+#include <version.h>
+#include <app_version.h>
 
 LOG_MODULE_REGISTER(oresat_zephyr_template, LOG_LEVEL_DBG);
 
+/** Dump Hardware Info
+ *    Show the unique processor id and the reason for reset.
+ *
+ *    This requires:
+ *    #include <zephyr/drivers/hwinfo.h>
+ *    prj.conf:
+ *    CONFIG_HWINFO=y
+ *    Names of reset cause bits defined in same header; e.g.
+ *    RESET_POR
+ **/
+static void dump_hwinfo(void)
+{
+	uint8_t hwid[32] = {0};
+	uint32_t reset_cause;
+	int reset_cause_ret;
+	int hwid_len;
+
+	hwid_len = hwinfo_get_device_id(hwid,sizeof(hwid)); // returns size of the device id
+	reset_cause_ret = hwinfo_get_reset_cause(&reset_cause);
+	if (!reset_cause_ret) {
+		hwinfo_clear_reset_cause();
+	}
+
+	if (hwid_len < 0) {
+		LOG_ERR("    Chip     HWID: UNNKOWN (%d", hwid_len);
+	} else {
+		LOG_HEXDUMP_INF(hwid, hwid_len, "    Chip     HWID: ");
+	}
+	if (reset_cause_ret < 0) {
+		LOG_ERR("      Reset Cause: UNKNOWN (%d)", reset_cause_ret);
+	} else {
+		LOG_INF("      Reset Cause: 0x%08x", reset_cause);
+	}
+}
+
 int main(void)
 {
-    LOG_INF("Oresat template starting on board: %s", CONFIG_BOARD_TARGET);
+	LOG_INF("Oresat Template App");
+	LOG_INF("   Oresat   Board: %s", CONFIG_BOARD_TARGET);
+	dump_hwinfo();
+	LOG_INF("   App    Version: %s", APP_VERSION_STRING);
+	LOG_INF("   Zephyr Version: %s", KERNEL_VERSION_STRING);
 
     return 0;
 }
